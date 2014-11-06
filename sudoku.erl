68a8b02bfb6830a7c8db5f0414aa6313468192e6
Dieter Schön <dieter.schoen@siemens.com>
2014-11-06 14:54:11 +0100

    basic documentation done

--- sudoku.erl
+++ sudoku.erl
@@ -1,19 +1,56 @@
 -module(sudoku).
+%% TODO: hide internals
 -compile(export_all).
 
+
+%%---------------------------------------------------------------------
+%% Data Type: cell
+%% where:
+%%    y: row of the sudoku cell
+%%    x: column of the sudoku cell
+%%    value: Initially a list with all possible values for this cell, i.e. [1..9]
+%%           Later, when the possibilities are limited by already determined
+%%           neighbours, the list gets shorter until this cell's value is
+%%           determined, then it is changed to a single integer 
+%%----------------------------------------------------------------------
 -record(cell, {y, x, value=[1,2,3,4,5,6,7,8,9]}).
 
+%%----------------------------------------------------------------------
+%% Function: start/0
+%% Purpose: spawn the master process, which in turn spawns the cells
+%% Args:   None
+%% Returns: A list of cell pids
+%%----------------------------------------------------------------------
 start() ->
     Pid = spawn(?MODULE, sudoku, [[]]),
     register(master, Pid),
     create_cells().
 
+%%----------------------------------------------------------------------
+%% Function: stop/0
+%% Purpose: stops all processes
+%% Args:   None
+%% Returns: Nothing
+%%----------------------------------------------------------------------
 stop() ->
     master ! stop.
 
-set(Y, X, Value) ->
+%%----------------------------------------------------------------------
+%% Function: set/3
+%% Purpose: stops all processes
+%% Args:   Y,X .. coordinates
+%%         Value .. 
+%% Returns: Nothing
+%%----------------------------------------------------------------------
+set(Y, X, Value) when is_integer(Value) ->
     master ! {set, Y, X, Value}.
 
+%%----------------------------------------------------------------------
+%% Function: get/0
+%% Purpose: fetches the current value from a cell
+%% Args:   Y,X .. coordinates
+%% Returns: Value, can be a list or an integer
+%%----------------------------------------------------------------------
 get(Y, X) ->
     master ! {get, self(), Y, X},
     receive
@@ -21,15 +58,41 @@ get(Y, X) ->
             V
     end.
 
+%%----------------------------------------------------------------------
+%% Function: printY/Y
+%% Purpose: gets the values of all cells in a row
+%% Args:   Y row identifier
+%% Returns: List of values
+%%----------------------------------------------------------------------
 printY(Y) ->
     [get(Y,X)|| X <- lists:seq(1,9)].
     
+%%----------------------------------------------------------------------
+%% Function: printCleanY/1
+%% Purpose: gets the values of all cells and replaces lists by '0'
+%%          this gives a more readable output
+%% Args:   Y row identifier
+%% Returns: List of cleaned values
+%%----------------------------------------------------------------------
 printCleanY(Y) ->
     lists:map(fun(X) -> if is_list(X) -> 0; true -> X end end, printY(Y)).
 
+%%----------------------------------------------------------------------
+%% Function: printClean/0
+%% Purpose: gets the cleaned values of all cells of the sudoku
+%% Args:   None
+%% Returns: List of lists of cell values
+%%----------------------------------------------------------------------
 printClean() ->
     [printCleanY(Y) || Y <- lists:seq(1,9)].
 
+%%----------------------------------------------------------------------
+%% Function: sudoku/1
+%% Purpose: master process
+%%          spawns the cells and acts as communication center
+%% Args:   List of cells, {tuple with Y,X and Pid}
+%% Returns: Nothing
+%%----------------------------------------------------------------------
 sudoku(Cells) ->
     receive
         {cell, Y, X, Pid} -> sudoku([{Y,X,Pid}|Cells]);
@@ -46,6 +109,13 @@ sudoku(Cells) ->
         stop -> broadcast(Cells, stop)
     end.
 
+%%----------------------------------------------------------------------
+%% Function: findCell/3
+%% Purpose: finds the cell with coordinates Y,X in the list of cells
+%% Args:   Y,X .. coordinates
+%%         Cells .. list of cells with {Y,X,Pid} elements
+%% Returns: Pid of cell or error if not found
+%%----------------------------------------------------------------------
 findCell(Y, X, [{Y,X,Pid}|_]) -> 
     Pid;
 findCell(_, _, []) -> 
@@ -53,20 +123,49 @@ findCell(_, _, []) ->
 findCell(Y,X, [_|Cells]) -> 
     findCell(Y,X,Cells).
  
+%%----------------------------------------------------------------------
+%% Function: broadcast/2
+%% Purpose: sends a message to all cells
+%% Args:   List of cells (as above), Message
+%% Returns: Nothing
+%%----------------------------------------------------------------------
 broadcast([{_Y,_X,Pid}|Cells], Message) ->
     Pid ! Message,
     broadcast(Cells, Message);
 broadcast([], _) -> 
     done.
     
+%%----------------------------------------------------------------------
+%% Function: create_cells/0
+%% Purpose: create all cells. Sudoku size of 9x9 is hardcoded
+%% Args:   None
+%% Returns: Nothing.
+%%----------------------------------------------------------------------
 create_cells() ->
     [create_cell(Y,X) || Y <- lists:seq(1,9),
-              X <- lists:seq(1,9)].
+                         X <- lists:seq(1,9)],
+    done.
 
+%%----------------------------------------------------------------------
+%% Function: create_cell/2
+%% Purpose: create one cell with given parameters
+%% Args:   Y,X .. coordinates
+%% Returns: Nothing
+%%----------------------------------------------------------------------
 create_cell(Y,X) ->
     Pid = spawn(?MODULE, cell, [#cell{y=Y,x=X}]),
-    master ! {cell, Y, X, Pid}.
+    master ! {cell, Y, X, Pid},
+    done.
 
+%%----------------------------------------------------------------------
+%% Function: cell/1
+%% Purpose: cell process loop
+%%          waits for messages from other cells and adapts own state,
+%%          if appropriate. Broadcasts own state, if value is fixed to
+%%          a single value.
+%% Args:   State record
+%% Returns: Nothing
+%%----------------------------------------------------------------------
 cell(#cell{y=Y, x=X, value=Value} = State) ->
     receive
         {set, V} ->
@@ -93,6 +192,13 @@ cell(#cell{y=Y, x=X, value=Value} = State) ->
         {stop} -> stop
     end.
 
+%%----------------------------------------------------------------------
+%% Function: isRelevant/0
+%% Purpose: used by cell/1 to determine if the message is of relevance for
+%%          the own state
+%% Args:   None
+%% Returns: Nothing
+%%----------------------------------------------------------------------
 isRelevant(Y,X,Y,X) ->
     % it's me
     false;
--- sudoku_test.erl
+++ sudoku_test.erl
@@ -2,6 +2,13 @@
 -export([do_test/0, do_test2/0]).
 -import(sudoku, [start/0, set/3]).
 
+
+%%----------------------------------------------------------------------
+%% Function: do_test/0
+%% Purpose: Test the sudoku implementation. This is a under-determined sudoku.
+%%          Verify afterwards with sudoku:printClean()
+%% Returns: done
+%%----------------------------------------------------------------------
 do_test() ->
     start(),
     set(1,5,1),
@@ -29,8 +36,15 @@ do_test() ->
     set(8,4,1),
     set(9,2,8),
     set(9,3,4),
-    set(9,5,2).
+    set(9,5,2),
+    done.
     
+%%----------------------------------------------------------------------
+%% Function: do_test2/0
+%% Purpose: Test the sudoku implementation. This is a well-determined sudoku.
+%%          Verify afterwards with sudoku:printClean()
+%% Returns: done
+%%----------------------------------------------------------------------
 do_test2() ->
     start(),
     set(1,3,8),
@@ -64,4 +78,5 @@ do_test2() ->
     set(8,8,8),
     set(9,4,4),
     set(9,5,3),
-    set(9,7,7).
+    set(9,7,7),
+    done.
